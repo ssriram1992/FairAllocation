3526435c07ae4305d84db5c61e50c8c1b06477f3
Philippe Olivier <philippe.olivier@polymtl.ca>
2020-05-11 10:51:37 -0400

    Quick clean-up.

--- ventilators.py
+++ ventilators.py
@@ -1,4 +1,5 @@
 import gurobipy as gp
+import matplotlib.pyplot as plt
 import ventutils
 
 
@@ -16,7 +17,8 @@ n = len(Q)
 T = len(Q[0])
 
 # D[i][j]: Delay in transfer from location i to location j.
-# TODO: Make this relative to the distance between locations (1000 miles = 1 day?)
+# TODO: Make this relative to the distance between locations (1000 miles = 1 day?).
+# Bertsimas chooses a lead time of 3 days.
 D = [[1 for _ in range(n)] for _ in range(n)]
 
 
@@ -26,14 +28,14 @@ D = [[1 for _ in range(n)] for _ in range(n)]
 
 # V[i]: Initial supply of ventilators at location i.
 V = ventutils.initial_supply()
-# Playing with values, can later be changed 
-temp = sum(V)
-V = [ int(vv/10) for vv in V]
+# TODO: Playing with values, can later be changed.
+V = [int(vv/10) for vv in V]
 
 # R: Recovery time.
 R = 10
 
-# N_max: Number of ventilators available to be injected into the system each day
+# N_max: Number of ventilators available to be injected into the system each day.
+# Bertsimas estimates 450 ventilators per day.
 N_max = 65000/T
 
 
@@ -47,7 +49,8 @@ M = gp.Model()
 x = M.addVars(n, T, lb=0, vtype=gp.GRB.INTEGER, name='x')
 
 # N[t]: Number of ventilators injected into the system at time t.
-N = M.addVars(T, lb=0.9*N_max, ub = N_max, vtype=gp.GRB.INTEGER, name='N')
+# The lower bound forces at least most of the ventilators to be used.
+N = M.addVars(T, lb=0.9*N_max, ub=N_max, vtype=gp.GRB.INTEGER, name='N')
 
 # z[i, t]: How many of the new ventilators N[t] are sent to location i at time t.
 z = M.addVars(n, T, lb=0, vtype=gp.GRB.INTEGER, name='z')
@@ -63,21 +66,19 @@ for t in range(-D_max, 0):
         for j in range(n):
             f[i, j, t].lb = 0
             f[i, j, t].ub = 0
-# No transfer to the same location!
+# Prevent transfers from a location to itself.
 for t in range(0, T):
     for i in range(n):
         f[i, i, t].lb = 0
         f[i, i, t].ub = 0
 
 # tau[i, t]: Shortage of ventilators at location i at time t.
-tau = M.addVars(n, T, lb=-GRB.INFINITY, vtype=gp.GRB.INTEGER, name='tau')
-tauPos = M.addVars(n, T, lb=0, vtype=gp.GRB.INTEGER, name='tauPos')
+tau = M.addVars(n, T, lb=-gp.GRB.INFINITY, vtype=gp.GRB.INTEGER, name='tau')
+tau_pos = M.addVars(n, T, lb=0, vtype=gp.GRB.INTEGER, name='tau_pos')
 
 # phi: Objective.
 phi = M.addVar(obj=1, lb=0, vtype=gp.GRB.INTEGER, name='phi')
 phi_ = M.addVars(n, lb=0, vtype=gp.GRB.INTEGER, name='phi_')
-
-
 
 
 ###############
@@ -90,18 +91,12 @@ phi_ = M.addVars(n, lb=0, vtype=gp.GRB.INTEGER, name='phi_')
 for t in range(T):
     M.addConstr(gp.quicksum(x[i, t] for i in range(n)) <=
                 sum(V) + gp.quicksum(N[tp] for tp in range(t+1)))
-#     M.addConstr(gp.quicksum(x[i, t] for i in range(n)) >=
-#                 Eff*sum(V) + Eff*gp.quicksum(N[tp] for tp in range(t+1)))
 
 # The number of new ventilators assigned at time t should not be greater than
 # the number of new ventilators injected into the system at time t.
 for t in range(T):
     M.addConstr(gp.quicksum(z[i, t] for i in range(n)) == N[t])
 
-# The total number of ventilators injected into the system must not be greater
-# than what is available to be injected.
-# M.addConstr(gp.quicksum(N[t] for t in range(T)) <= N_max)
-    
 # The number of ventilators in the various locations should be consistent over
 # time, including consistency with the new ventilators injected into the system
 # and with the transfer of ventilators between locations.
@@ -122,19 +117,16 @@ for i in range(n):
 # Keep track of the shortages of ventilators.
 for i in range(n):
     for t in range(T):
-        # No max needed since LB of tau is already 0
         M.addConstr(tau[i, t] == Q[i][t]-x[i, t])
-        M.addGenConstrMax(tauPos[i,t],[tau[i,t]],0)
+        M.addGenConstrMax(tau_pos[i, t], [tau[i, t]], 0)
         
 
-        
-        
 #############
 # OBJECTIVE #
 #############
 
 for i in range(n):
-    M.addConstr(phi_[i] == gp.quicksum(tauPos[i, t] for t in range(T)))
+    M.addConstr(phi_[i] == gp.quicksum(tau_pos[i, t] for t in range(T)))
     
 M.addGenConstrMax(phi, phi_)
 
@@ -147,7 +139,6 @@ M.update()
 M.optimize()
 
 
-
 ############
 # PLOTTING #
 ############
@@ -156,9 +147,9 @@ def plotState(loc):
     xval = [t for t in range(T)]
     yval = [x[loc, t].X for t in range(T)]
     fig = plt.figure()
-    ax = fig.add_axes([0,0,1.2,1.2])
-    ax.plot(xval,yval,'bo--')
-    ax. plot(xval, [Q[loc][t] for t in range(T)],'rs--')
+    ax = fig.add_axes([0, 0, 1.2, 1.2])
+    ax.plot(xval, yval, 'bo--')
+    ax. plot(xval, [Q[loc][t] for t in range(T)], 'rs--')
     ax.legend(labels = ('Quantity supplied', 'Quantity demanded'), loc = 'upper right')
     ax.set_title("State "+str(loc))
     ax.set_xlabel('Day')
@@ -167,4 +158,3 @@ def plotState(loc):
     
 for loc in range(50):
     plotState(loc)
-
