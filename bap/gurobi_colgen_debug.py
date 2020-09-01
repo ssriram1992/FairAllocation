4b0091f2d93b89fd9fdca2576ffd031de630fb93
Philippe Olivier <philippe.olivier@polymtl.ca>
2020-09-01 12:40:31 -0400

    Modified debug file with a small example

--- bap/gurobi_colgen_debug.py
+++ bap/gurobi_colgen_debug.py
@@ -6,54 +6,75 @@ import gurobipy as gp
 
 EPS = 1e-6
 
-# For efficiency we use the performance target for the Netherlands, i.e.,
-# that "95% of all calls should be reached within 15 minutes (900 seconds)".
-utrecht = Utrecht()
-utrecht.reduce_graph(900)
+# # For efficiency we use the performance target for the Netherlands, i.e.,
+# # that "95% of all calls should be reached within 15 minutes (900 seconds)".
+# utrecht = Utrecht()
+# utrecht.reduce_graph(900)
 
-# Bases are the only vertices that can be allocated ambulances.
-bases = utrecht.B
-n = len(utrecht.V)
+# # Bases are the only vertices that can be allocated ambulances.
+# bases = utrecht.B
+# n = len(utrecht.V)
 
-# Replace the seconds by a boolean representing if the zones are close enough
-# to cover each other or not.
-adj = utrecht.get_binary_adjacency()
+# # Replace the seconds by a boolean representing if the zones are close enough
+# # to cover each other or not.
+# adj = utrecht.get_binary_adjacency()
 
-# Sufficient coverage required for various population densities.
-sufficient = utrecht.get_sufficient_coverage()
+# # Sufficient coverage required for various population densities.
+# sufficient = utrecht.get_sufficient_coverage()
+
+# SMALL EXAMPLE
+bases = [0, 1, 2, 3, 4] # All zones are bases
+n = len(bases)
+
+# The graph looks like: A-B-C-D
+#                           |
+#                           E
+
+adj = [[1, 1, 0, 0, 0],
+       [1, 1, 1, 0, 0],
+       [0, 1, 1, 1, 1],
+       [0, 0, 1, 1, 0],
+       [0, 0, 1, 0, 1]]
+adj = numpy.matrix(adj)
+
+sufficient = [2, 2, 2, 2, 2] # Each zone needs to be covered by 2 ambulances. This means that for min_coverage=0.4, all columns where 2 ambulances are allocated to the same node are valid, but also that some other columns where ambulances are allocated to 2 different nodes may also be valid in some cases
 
 # Model parameters
-num_ambulances = 20
+num_ambulances = 2
 num_rounds = 3
 max_transition = 1 # 0 = no transition allowed, 1 = unlimited transitions
-min_coverage = 0.95 # 0 = no one needs to be covered, 1 = everyone has to be covered
-max_practical_ambulances = 4 # Maximum practical number of ambulances in a zone (doesn't seem to make much of a difference). This is 4 because a base with 4 ambulances can sufficiently cover any zone no matter what its population density
+min_coverage = 0.4 # 0 = no one needs to be covered, 1 = everyone has to be covered
+max_practical_ambulances = 2 # Maximum practical number of ambulances in a zone (doesn't seem to make much of a difference). This is 4 because a base with 4 ambulances can sufficiently cover any zone no matter what its population density
 
 
-# Find one feasible configuration for the initial column.
-def initial_feasible_configuration():
-    """Returns one feasible allocation, along with its coverage."""
-    m = gp.Model('M')
-    x = [m.addVar(lb=0, ub=max_practical_ambulances, vtype='I', name='x'+str(i)) for i in range(len(bases))]
+# # Find one feasible configuration for the initial column.
+# def initial_feasible_configuration():
+#     """Returns one feasible allocation, along with its coverage."""
+#     m = gp.Model('M')
+#     x = [m.addVar(lb=0, ub=max_practical_ambulances, vtype='I', name='x'+str(i)) for i in range(len(bases))]
 
-    # Constraint: Don't use more than the available number of ambulances.
-    m.addConstr(gp.quicksum(x) <= num_ambulances)
+#     # Constraint: Don't use more than the available number of ambulances.
+#     m.addConstr(gp.quicksum(x) <= num_ambulances)
 
-    # Constraint: The configuration must sufficiently cover 95% of the zones.
-    c = [m.addVar(vtype='B', name='c'+str(i)) for i in range(n)]
-    for i in range(n):
-        m.addGenConstrIndicator(c[i], True, gp.quicksum(x[j] * adj[bases[j], i] for j in range(len(bases))) >= sufficient[i])
-    m.addConstr(gp.quicksum(c) >= math.ceil(min_coverage*n))
-    m.update()
-    m.optimize()
+#     # Constraint: The configuration must sufficiently cover 95% of the zones.
+#     c = [m.addVar(vtype='B', name='c'+str(i)) for i in range(n)]
+#     for i in range(n):
+#         m.addGenConstrIndicator(c[i], True, gp.quicksum(x[j] * adj[bases[j], i] for j in range(len(bases))) >= sufficient[i])
+#     m.addConstr(gp.quicksum(c) >= math.ceil(min_coverage*n))
+#     m.update()
+#     m.optimize()
 
-    allocation = [m.getVarByName('x'+str(i)).X for i in range(len(x))]
-    coverage = utrecht.allocation_coverage(allocation)
+#     allocation = [m.getVarByName('x'+str(i)).X for i in range(len(x))]
+#     coverage = utrecht.allocation_coverage(allocation)
 
-    return allocation, coverage
+#     return allocation, coverage
 
 # Initial feasible column.
-allocation, coverage = initial_feasible_configuration()
+# allocation, coverage = initial_feasible_configuration()
+
+# Initial column
+allocation = [2, 0, 0, 0, 0]
+coverage = [2, 2, 0, 0, 0]
 
 # Keeping track of allocations and coverages.
 allocations = [allocation]
